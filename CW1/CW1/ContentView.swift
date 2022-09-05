79dae062adfe0a18d900b6eaf40a670e9603c1fe
alsharhan98 <alaa6249@gmail.com>
2022-09-05 20:40:41 +0300

    submit

--- CW1/CW1/ContentView.swift
+++ CW1/CW1/ContentView.swift
@@ -2,20 +2,64 @@
 //  ContentView.swift
 //  CW1
 //
-//  Created by user on 05/09/2022.
+//  Created by aalaa alsharhan on 05/09/2022.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("newi")
+                .resizable()
+                
+              
+            VStack{
+            Text("Hello, my name is aalaa")
+                    .font(.custom("Baskerville", size: 30))
+                    .fontWeight(Font.Weight.bold)
+                    .foregroundColor(.white)
+                  
+                    .padding()
+            Text("I am 24 years old")
+                    .font(.custom("Baskerville", size: 22))
+                    .fontWeight(Font.Weight.bold)
+                    .foregroundColor(.white)
+                    .padding()
+            Text("I am learning SwiftUI now")
+                    .font(.custom("Baskerville", size: 15))
+                    .fontWeight(Font.Weight.bold)
+                    .foregroundColor(.white)
+                    .padding()
+                
+                HStack{
+                   
+                  
+                   Image(systemName:"heart")
+                        .font(.system(size : 30,weight: .bold))
+                        .foregroundColor(.white)
+                        .padding()
+                        Spacer()
+                    Image(systemName:"gear")
+                        .font(.system(size : 30,weight: .bold))
+                        .foregroundColor(.white)
+                        Spacer()
+                    Image(systemName:"trash")
+                        .font(.system(size : 30,weight: .bold))
+                        .foregroundColor(.white)
+                        .padding()
+                }
+        }
+       
+        }
+        .ignoresSafeArea()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13 Pro")
     }
 }
